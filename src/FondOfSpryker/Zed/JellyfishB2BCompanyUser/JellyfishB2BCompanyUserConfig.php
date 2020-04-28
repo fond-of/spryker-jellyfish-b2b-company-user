e5f19c36d1cc5d5f800812445968a3c4863eb836
Jozsef Geng <gengjozsef86@gmail.com>
2020-04-28 10:39:26 +0200

    code cleanup and remove CompanyTypeRoleConstants dependency

--- src/FondOfSpryker/Zed/JellyfishB2BCompanyUser/JellyfishB2BCompanyUserConfig.php
+++ src/FondOfSpryker/Zed/JellyfishB2BCompanyUser/JellyfishB2BCompanyUserConfig.php
@@ -2,7 +2,7 @@
 
 namespace FondOfSpryker\Zed\JellyfishB2BCompanyUser;
 
-use FondOfSpryker\Shared\CompanyTypeRole\CompanyTypeRoleConstants;
+use FondOfSpryker\Shared\JellyfishB2BCompanyUser\JellyfishB2BCompanyUserConstants;
 use Spryker\Zed\Kernel\AbstractBundleConfig;
 
 class JellyfishB2BCompanyUserConfig extends AbstractBundleConfig
@@ -14,7 +14,7 @@ class JellyfishB2BCompanyUserConfig extends AbstractBundleConfig
      */
     public function getValidCompanyRolesForExport(string $companyType = ''): array
     {
-        $companyRoles = $this->get(CompanyTypeRoleConstants::VALID_COMPANY_ROLES_FOR_EXPORT);
+        $companyRoles = $this->get(JellyfishB2BCompanyUserConstants::VALID_COMPANY_ROLES_FOR_EXPORT);
 
         if ($companyType === '') {
             return $companyRoles;
